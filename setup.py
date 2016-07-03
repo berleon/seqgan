e30f617ea5d63bd007bc261b9b51e3e77734520a
berleon <github@leon-sixt.de>
2016-07-03 14:12:28 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+dep_links = [str(req_line.url) for req_line in install_reqs]
+
+setup(
+    name='seqgan',
+    version='0.0.1',
+    description='',
+    install_requires=reqs,
+    dependency_links=dep_links,
+    packages=['seqgan']
+)
